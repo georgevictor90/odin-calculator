344f9fbe68b1784f43336156e79c806331a7dcd6
georgevictor90 <georgevictorl@yahoo.com>
2021-11-24 00:59:37 +0200

    rewrote functions to be easier to read

--- script.js
+++ script.js
@@ -1,168 +1,161 @@
-// Grab the buttons
+// Grab the elements
 const numButtons = Array.from(document.querySelectorAll('.num-btn'));
+const opButtons = Array.from(document.querySelectorAll('.operator-btn'));
 const acBtn = document.getElementById('ac');
 const delBtn = document.getElementById('del');
-const opButtons = Array.from(document.querySelectorAll('.operator-btn'));
 const equalBtn = document.querySelector('.equal');
-const display = document.querySelector('.display');
+const displayPreviousOperand = document.querySelector('[data-previous-operand]');
+const displayCurrentOperand = document.querySelector('[data-current-operand]');
 
 //Regex looking for operator symbols
 const exp = /\D+/;
 
 //Set defaults
-let num1 = '';
-let num2 = ''; 
-let operator = '';
-let result = '';
-let lastResult = '';
-display.textContent = '0';
-
+let operator = undefined;
+let currentOperand = '';
+let previousOperand = '';
 
 // Add functions for the basic operations
 const add = function(a, b) {
   return a + b;
-  }
-  
-  const subtract = function(a, b) {
-    return a - b;
-  }
-  
-  const multiply = function(a, b) {
-    return a * b;
-  }
-  
-  const divide = function(a, b) {
-    return a / b;
-  }
-  
-  
-// Add function operate that takes an operator and two numbers and calls one of the above functions on the number
-  const operate = function(operator, num1, num2) {
-    switch(operator) {
-      case "+":
-        return add(num1,num2);
-      case "-":
-        return subtract(num1, num2);
-      case "*":
-        return multiply(num1, num2);
-      case "/":
-        return divide(num1, num2);
-    }    
-  }
-
-// Add event listeners for number Buttons
-for (let i = 0; i < numButtons.length; i++) {
-  numButtons[i].addEventListener('click', setNumber);
-
-  function setNumber() {
-    if (typeof num1 === 'string') {
-      num1 = Number(numButtons[i].value);
-      display.textContent = num1.toString();
-
-    } else if (typeof num1 === 'number') {
-        if (exp.test(display.textContent)) {
-          if (num2 === '') {
-            num2 = Number(numButtons[i].value);
-          
-          } else if (typeof num2 === 'number') {
-            //ADD ANOTHER DIGIT TO NUM2
-            num2 = Number(num2.toString().concat(numButtons[i].value));
-            
-          }
-          display.textContent = num1 + operator + num2;
-        
-        } else {
-          // ADD ANOTHER DIGIT TO NUM1
-          num1 = Number(num1.toString().concat(numButtons[i].value));
-          display.textContent = num1.toString();
-        }
-    }
-  }
 }
 
-
-  
-// Add event listeners for operator buttons
-for (let i = 0; i < opButtons.length; i++) {
-  opButtons[i].addEventListener('click', setOperator);
-  function setOperator() {
-    if (num1 === '' && operator === '') {
-      num1 = 0;
-    } else if (typeof num1 === 'number' && operator.length > 0 && typeof num2 === 'number') {
-      solveOperation();
-      
-    } 
-    operator = opButtons[i].value;
-    display.textContent = num1 + operator; 
-  };
+const subtract = function(a, b) {
+  return a - b;
 }
 
-// ADD EVENT LISTENER FOR EQUALS BUTTON
-equalBtn.addEventListener('click', solveOperation); 
-function solveOperation() {
-  if (typeof num1 === 'number' && typeof num2 === 'number') {
-    result = operate(operator, num1, num2);
-    display.textContent = result.toString();
-    num2 = '';
-    lastResult = result;
-    num1 = lastResult;
-    console.log(num1);
-    operator = '';
-  }
+const multiply = function(a, b) {
+  return a * b;
+}
+
+const divide = function(a, b) {
+  return a / b;
+}
+
+const percentage = function(a, b) {
+  return a / 100 * b;
 }
 
 
 
 
-acBtn.addEventListener('click',  clearAll);
+function allClear() {
+  currentOperand = '';
+  previousOperand = '';
+  operator = undefined;
+}
 
-function clearAll() {
-  display.textContent = '0';
-  num1 = '';
-  num2 = '';
-};
+function del() {
+  currentOperand = currentOperand.toString().slice(0, -1);
+}
 
-delBtn.addEventListener('click', deleteLastIndex);
+function operate() {
+ let result;
+ const previousNum = parseFloat(previousOperand); 
+ const currentNum = parseFloat(currentOperand); // i am using parseFloat() instead of Number() because the latter converts an empty string to 0 instead of NaN;
+ if (isNaN(previousNum) || isNaN(currentNum)) return;
 
+ switch(operator) {
+  case "+":
+    result = add(previousNum, currentNum);
+    break;
+  case "-":
+    result = subtract(previousNum, currentNum);
+    break;
+  case "×":
+    result = multiply(previousNum, currentNum);
+    break;
+  case "÷":
+    result = divide(previousNum, currentNum);
+    break;
+  case "%":
+    result = percentage(previousNum, currentNum);
+    break;
+  default: 
+    return;
+}    
+ currentOperand = result;
+ operator = undefined;
+ previousOperand = '';   
+}
 
+function appendNumber(number) {
+  if (number === '.' && currentOperand.includes('.')) return
+  currentOperand = currentOperand.toString() + number.toString();
+}
 
-function deleteLastIndex() {
-  if (num2.toString().length < 1) {
-  
-    if (operator.length > 0) {
-      operator = clearItem(operator);
-      display.textContent = num1;
-  
-    } else {
-    
-      if (num1.toString().length <= 1) {
-        clearAll();
-    
-      } else {
-        num1 = trimNumber(num1);
-        display.textContent = num1;
-    
-      }
-    }
-  
-  } else if (num2.toString().length == 1) {
-    num2 = clearItem(num2);
-    display.textContent = num1 + operator;
-  
-  } else if (num2.toString().length > 1) {
-      num2 = trimNumber(num2);
-      display.textContent = num1 + operator + num2;
-  
+function getDisplayNumber(number) {
+  const stringNumber = number.toString();
+  const integerDigits = parseFloat(stringNumber.split('.')[0]);
+  const decimalDigits = stringNumber.split('.')[1];
+  let integerDisplay;
+  if (isNaN(integerDigits)) {
+    integerDisplay = '';
+  } else {
+    integerDisplay = integerDigits.toLocaleString('en', {
+    maximumFractionDigits: 0})
+  }
+
+  if (decimalDigits != null) {
+    return `${integerDisplay}.${decimalDigits}`;
+  } else {
+    return integerDisplay;
   }
 }
-  
-  
 
-
-function trimNumber(numb) {
-  return numb = Number(numb.toString().substring(0, numb.toString().length - 1))
+function roundNum(number) {
+  return number = Math.round((number + Number.EPSILON) * 100) / 100;
 }
 
-function clearItem(item) {
-  return item = '';
+function updateDisplay() {
+  displayCurrentOperand.textContent = getDisplayNumber(currentOperand);
+  if (operator !== undefined) {
+    displayPreviousOperand.textContent = `${getDisplayNumber(previousOperand)} ${operator}`;
+  } else {
+    displayPreviousOperand.textContent = '';
+  }
 }
+
+function selectOperation(operation) {
+  if (currentOperand === '') return
+  if (previousOperand !== '') {
+    operate();
+  }
+  operator = operation;
+  previousOperand = currentOperand;
+  currentOperand = '';
+}
+
+// ADD EVENT LISTENERS FOR NUMBER BUTTONS
+numButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    appendNumber(button.textContent);
+    updateDisplay();
+  })
+})
+
+//ADD EVENT LISTENERS FOR OPERATION BUTTONS
+opButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    selectOperation(button.textContent);
+    updateDisplay();
+  })
+})
+
+//ADD EVENT LISTENER FOR EQUALS BUTTON
+equalBtn.addEventListener('click', button => {
+  operate();
+  updateDisplay();
+})
+
+//ADD EVENT LISTENER FOR ALL CLEAR BUTTON
+acBtn.addEventListener('click', button => {
+  allClear();
+  updateDisplay();
+})
+
+//ADD EVENT LISTENER FOR DELETE BUTTON
+delBtn.addEventListener('click', button => {
+  del();
+  updateDisplay();
+})
