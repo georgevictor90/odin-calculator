4ac073a07ffafcdfa19c108e49bc9d2933348aa0
georgevictor90 <georgevictorl@yahoo.com>
2021-11-24 22:59:22 +0200

    added keyboard support

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-// Grab the elements
+// GRAB THE ELEMENTS
 const numButtons = Array.from(document.querySelectorAll('.num-btn'));
 const opButtons = Array.from(document.querySelectorAll('.operator-btn'));
 const acBtn = document.getElementById('ac');
 const delBtn = document.getElementById('del');
 const equalBtn = document.querySelector('.equal');
+const negateBtn = document.querySelector('.negate-btn');
 const displayPreviousOperand = document.querySelector('[data-previous-operand]');
 const displayCurrentOperand = document.querySelector('[data-current-operand]');
 
-//Regex looking for operator symbols
-const exp = /\D+/;
 
-//Set defaults
+
+//SET DEFAULTS
 let operator = undefined;
 let currentOperand = '';
 let previousOperand = '';
 
-// Add functions for the basic operations
+// ADD FUNCTIONS FOR BASIC OPERATIONS
 const add = function(a, b) {
   return roundNum(a + b);
 }
@@ -37,96 +37,29 @@ const percentage = function(a, b) {
 }
 
 
+const numRegex = /\d|\./g;
+const opRegex = /\+|\-|\*|\//g;
 
-
-function allClear() {
-  currentOperand = '';
-  previousOperand = '';
-  operator = undefined;
-}
-
-function del() {
-  currentOperand = currentOperand.toString().slice(0, -1);
-}
-
-function operate() {
- let result;
- const parsedPreviousOperand = parseFloat(previousOperand);
- const parsedCurrentOperand = parseFloat(currentOperand);
- const previousNum = roundNum(parsedPreviousOperand); 
- const currentNum = roundNum(parsedCurrentOperand); // i am using parseFloat() instead of Number() because the latter converts an empty string to 0 instead of NaN;
- if (isNaN(previousNum) || isNaN(currentNum)) return;
-
- switch(operator) {
-  case "+":
-    result = add(previousNum, currentNum);
-    break;
-  case "-":
-    result = subtract(previousNum, currentNum);
-    break;
-  case "×":
-    result = multiply(previousNum, currentNum);
-    break;
-  case "÷":
-    result = divide(previousNum, currentNum);
-    break;
-  case "%":
-    result = percentage(previousNum, currentNum);
-    break;
-  default: 
-    return;
-}    
- currentOperand = result;
- operator = undefined;
- previousOperand = '';   
-}
-
-function appendNumber(number) {
-  if (number === '.' && currentOperand.includes('.')) return
-  currentOperand = currentOperand.toString() + number.toString();
-}
-
-function getDisplayNumber(number) {
-  const stringNumber = number.toString();
-  const integerDigits = parseFloat(stringNumber.split('.')[0]);
-  const decimalDigits = stringNumber.split('.')[1];
-  let integerDisplay;
-  if (isNaN(integerDigits)) {
-    integerDisplay = '';
-  } else {
-    integerDisplay = integerDigits.toLocaleString('en', {
-    maximumFractionDigits: 0})
-  }
-
-  if (decimalDigits != null) {
-    return `${integerDisplay}.${decimalDigits}`;
-  } else {
-    return integerDisplay;
-  }
-}
-
-function roundNum(number) {
-  return number = Math.round((number + Number.EPSILON) * 100) / 100;
-}
-
-function updateDisplay() {
-  displayCurrentOperand.textContent = getDisplayNumber(currentOperand);
-  if (operator !== undefined) {
-    displayPreviousOperand.textContent = `${getDisplayNumber(previousOperand)} ${operator}`;
-  } else {
-    displayPreviousOperand.textContent = '';
-  }
-}
-
-function selectOperation(operation) {
-  if (currentOperand === '') return
-  if (previousOperand !== '') {
+//ADD EVENT LISTENER FOR USER KEYPRESS
+document.addEventListener('keypress', (e) => {
+  const keyPressed = e.key;
+  if (keyPressed === 'Enter') {
     operate();
+    updateDisplay();
   }
-  operator = operation;
-  previousOperand = currentOperand;
-  currentOperand = '';
-}
+  if (keyPressed === 'Delete') {
+    allClear();
+    updateDisplay();
+  }
+  if (numRegex.test(keyPressed)) {
+    appendNumber(keyPressed);
+    updateDisplay();
+  }
+  if (opRegex.test(keyPressed)) {
+    selectOperation(keyPressed);
+    updateDisplay();
+  }
+});
 
 // ADD EVENT LISTENERS FOR NUMBER BUTTONS
 numButtons.forEach(button => {
@@ -139,7 +72,7 @@ numButtons.forEach(button => {
 //ADD EVENT LISTENERS FOR OPERATION BUTTONS
 opButtons.forEach(button => {
   button.addEventListener('click', () => {
-    selectOperation(button.textContent);
+    selectOperation(button.value);
     updateDisplay();
   })
 })
@@ -161,3 +94,118 @@ delBtn.addEventListener('click', button => {
   del();
   updateDisplay();
 })
+
+//ADD EVENT LISTENER FOR NEGATE BUTTON
+negateBtn.addEventListener('click', button => {
+  negateNumber();
+  updateDisplay();
+})
+
+
+
+//ADD FUNCTIONS FOR EVENT LISTENERS
+function allClear() {
+  currentOperand = '';
+  previousOperand = '';
+  operator = undefined;
+}
+
+
+function del() {
+  currentOperand = currentOperand.toString().slice(0, -1);
+}
+
+
+function operate() {
+ let result;
+ const parsedPreviousOperand = parseFloat(previousOperand);
+ const parsedCurrentOperand = parseFloat(currentOperand);
+ const previousNum = roundNum(parsedPreviousOperand); 
+ const currentNum = roundNum(parsedCurrentOperand); // i am using parseFloat() instead of Number() because the latter converts an empty string to 0 instead of NaN;
+ if (isNaN(previousNum) || isNaN(currentNum)) return;
+
+ switch(operator) {
+  case "+":
+    result = add(previousNum, currentNum);
+    break;
+  case "-":
+    result = subtract(previousNum, currentNum);
+    break;
+  case "*":
+    result = multiply(previousNum, currentNum);
+    break;
+  case "/":
+    result = divide(previousNum, currentNum);
+    break;
+  case "%":
+    result = percentage(previousNum, currentNum);
+    break;
+  default: 
+    return;
+}    
+ currentOperand = result;
+ operator = undefined;
+ previousOperand = '';   
+}
+
+
+function appendNumber(number) {
+  if (number === '.' && currentOperand.includes('.')) return
+  currentOperand = currentOperand.toString() + number.toString();
+}
+
+
+function getDisplayNumber(number) {
+  const stringNumber = number.toString();
+  const integerDigits = parseFloat(stringNumber.split('.')[0]);
+  const decimalDigits = stringNumber.split('.')[1];
+  let integerDisplay;
+  if (isNaN(integerDigits)) {
+    integerDisplay = '';
+  } else {
+    integerDisplay = integerDigits.toLocaleString('en', {
+    maximumFractionDigits: 0})
+  }
+
+  if (decimalDigits != null) {
+    return `${integerDisplay}.${decimalDigits}`;
+  } else {
+    return integerDisplay;
+  }
+}
+
+
+function roundNum(number) {
+  return number = Math.round((number + Number.EPSILON) * 100) / 100;
+}
+
+
+function negateNumber() {
+  if (currentOperand === '') return;
+  if (currentOperand.toString().includes('-')) {
+    return currentOperand = parseFloat(currentOperand.toString().slice(1))
+  } else {
+    return currentOperand = parseFloat('-' + currentOperand.toString());
+  }
+}
+
+
+function updateDisplay() {
+  displayCurrentOperand.textContent = getDisplayNumber(currentOperand);
+  if (operator !== undefined) {
+    displayPreviousOperand.textContent = `${getDisplayNumber(previousOperand)} ${operator}`;
+  } else {
+    displayPreviousOperand.textContent = '';
+  }
+}
+
+
+function selectOperation(operation) {
+  if (currentOperand === '') return
+  if (previousOperand !== '') {
+    operate();
+  }
+  operator = operation;
+  previousOperand = currentOperand;
+  currentOperand = '';
+}
